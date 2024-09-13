5c95fd86fe97f16c99defa91a984c804fe6873aa
Myster7494 <jackychiu0101@gmail.com>
2024-09-14 00:47:26 +0800

    feat: add Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+// class 名稱必須跟檔案名稱一樣
+public class Main {
+
+    // Java 程式需要一個主方法(main 方法)，程式從這裡開始執行
+    public static void main(String[] args) {
+
+        // 在 Java 中，使用 System.out.println() 來輸出資料
+        System.out.println("Hello, World!");
+
+    }
+}
